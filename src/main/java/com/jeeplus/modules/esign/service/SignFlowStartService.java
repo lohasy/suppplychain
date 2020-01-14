09e7e89fa3161758ec72e2410368be272bfd48a8
Jax <lixiang@cx-gyl.com>
2020-01-14 22:51:53 +0800

    回调接口完成

--- src/main/java/com/jeeplus/modules/esign/service/SignFlowStartService.java
+++ src/main/java/com/jeeplus/modules/esign/service/SignFlowStartService.java
@@ -1,10 +1,13 @@
 package com.jeeplus.modules.esign.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jeeplus.modules.esign.bean.ServerResponse;
 import com.jeeplus.modules.esign.exception.DefineException;
 
 public interface SignFlowStartService {
 
     ServerResponse signFlowStart(String flowId) throws DefineException;
+
+    void callBackSave(JSONObject json);
 
 }
