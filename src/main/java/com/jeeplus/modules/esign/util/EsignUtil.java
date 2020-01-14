8ee53209f6fd7bd0a302d2b610ef0d393e89ed11
pengwei <pw09066310@163.com>
2020-01-14 09:29:32 +0800

    提交accesstoken

---
+++ src/main/java/com/jeeplus/modules/esign/util/EsignUtil.java
@@ -0,0 +1,78 @@
+package com.jeeplus.modules.esign.util;
+
+import com.alibaba.fastjson.JSONObject;
+import com.jeeplus.modules.esign.bean.AccessToken;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+/**
+ * @author lohas
+ */
+public class EsignUtil {
+    private static final String GET_TOKEN_URL = "https://smlopenapi.esign.cn/v1/oauth2/access_token?appId=APPID&secret=APPSECRET&grantType=client_credentials";
+    private static final String APPID = "4438793080";
+    private static final String APPSECRET = "46748dcae0a380f46e75e0e9b7b284e3";
+    /**
+     * 存储token和过期时间
+     */
+    private static AccessToken at;
+
+    /**
+     * 获取token
+     */
+    private static void getToken(){
+        String url = GET_TOKEN_URL.replace("APPID",APPID).replace("APPSECRET",APPSECRET);
+        String tokenStr = getTokenStr(url);
+        System.out.println(tokenStr);
+        JSONObject jsonObject = JSONObject.parseObject(tokenStr);
+        JSONObject json = jsonObject.getJSONObject("data");
+        String token = json.getString("token");
+        String expireIn = json.getString("expiresIn");
+        //创建token，并存储起来
+        at = new AccessToken(token,expireIn);
+    }
+
+    /**
+     * 向外暴露的获取token的方法
+     * @return
+     */
+    public static String getAccessToken(){
+        if(at == null||at.isExpired()){
+            getToken();
+        }
+        return at.getAccessToken();
+    }
+
+    /**
+     * 调用e签宝的token接口
+     * @param url
+     * @return
+     */
+    private static String getTokenStr(String url) {
+        try{
+            URL urlObj = new URL(url);
+            URLConnection conn = urlObj.openConnection();
+            InputStream inputStream = conn.getInputStream();
+            byte[] b = new byte[1024];
+            int len;
+            StringBuilder sb = new StringBuilder();
+            while ((len = inputStream.read(b)) != -1){
+                sb.append(new String(b,0,len));
+            }
+            return  sb.toString();
+        }catch (Exception e){
+        e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        String accessToken = getAccessToken();
+        System.out.println(accessToken);
+        System.out.println(getAccessToken());
+    }
+}
