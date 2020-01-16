a9b48896fb4a44aba1a39b20179170aa8cb14d03
pengwei <pw09066310@163.com>
2020-01-16 14:59:33 +0800

    加上注释

--- src/main/java/com/jeeplus/modules/esign/service/FaceService.java
+++ src/main/java/com/jeeplus/modules/esign/service/FaceService.java
@@ -3,9 +3,10 @@ package com.jeeplus.modules.esign.service;
 import com.jeeplus.modules.esign.bean.FaceResultDto;
 
 public interface FaceService {
+    //todo 获取人脸地址
     Object getFaceUrl();
-
+    //todo 获取人脸识别结果
     Object faceResult(FaceResultDto faceResultDto);
-
+    //todo 获取accesstokn e签宝
     Object getAccessToken();
 }
